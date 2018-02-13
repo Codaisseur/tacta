d7cce672b9dae181f604c6783cd637f92320410a
michfarr <etnunc@protonmail.com>
2018-02-13 12:43:57 +0100

    Delete contacts

--- tacta.rb
+++ tacta.rb
@@ -57,17 +57,32 @@ def action_new(contacts)
   puts
 end
 
+def action_delete(contacts)
+  puts
+  response = ask("Delete which contact? ")
+
+  index = response.to_i
+
+  puts
+  puts "Contact for #{contacts[index - 1][:name]} deleted."
+
+  contacts.delete_at(index - 1)
+  puts
+end
+
 loop do
   index(contacts)
 
   puts
-  response_options = "(n for new, q to quit)"
+  response_options = "(n for new, d for delete, q to quit)"
   response = ask("Who would you like to see #{response_options}? ")
 
   break if response == "q"
 
   if response == "n"
     action_new(contacts)
+  elsif response == "d"
+    action_delete(contacts)
   else
     action_show(contacts, response.to_i)
   end
