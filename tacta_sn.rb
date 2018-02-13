19875057766caf3f86265b3786896b6e8d2f2a0f
michfarr <etnunc@protonmail.com>
2018-02-13 14:48:50 +0100

    Add 'PATCH /contacts/:idx' with view for update action

--- tacta_sn.rb
+++ tacta_sn.rb
@@ -28,6 +28,14 @@ get '/contacts/:index' do
   erb :'contacts/show'
 end
 
+get '/contacts/:index/edit' do
+  @index = params[:index].to_i
+  contacts = read_contacts
+  @contact = contacts[@index]
+
+  erb :'contacts/edit'
+end
+
 # POST /contacts
 post '/contacts' do
   new_contact = {
@@ -41,6 +49,22 @@ post '/contacts' do
   write_contacts(contacts)
 
   index = contacts.length - 1
+
+  redirect "/contacts/#{index}"
+end
+
+patch '/contacts/:index' do
+  index = params[:index].to_i
+
+  updated_contact = {
+    name:  params[:name],
+    phone: params[:phone],
+    email: params[:email]
+  }
+
+  contacts = read_contacts
+  contacts[index] = updated_contact
+  write_contacts(contacts)
 
   redirect "/contacts/#{index}"
 end
