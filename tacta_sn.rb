d65754747960d993218dd7eb79cbafb8ca78d1e0
michfarr <etnunc@protonmail.com>
2018-02-13 13:24:32 +0100

    Add show action and view

--- tacta_sn.rb
+++ tacta_sn.rb
@@ -10,5 +10,15 @@ end
 # GET /contacts
 get '/contacts' do
   @contacts = read_contacts
+
   erb :'contacts/index'
+end
+
+# GET /contacts/:index
+get '/contacts/:index' do
+  @index = params[:index].to_i
+  contacts = read_contacts
+  @contact = contacts[@index]
+
+  erb :'contacts/show'
 end
