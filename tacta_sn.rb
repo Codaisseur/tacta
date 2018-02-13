8f357d02dd0817c7a10682ee9b826fd8e94f35e1
michfarr <etnunc@protonmail.com>
2018-02-13 13:16:04 +0100

    Add index action for 'GET /contacts'

--- tacta_sn.rb
+++ tacta_sn.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 
+require './contacts_file'
+
 # GET /
 get '/' do
   "<h1>Tacta Contact Manager</h1>"
+end
+
+# GET /contacts
+get '/contacts' do
+  @contacts = read_contacts
+  erb :'contacts/index'
 end
